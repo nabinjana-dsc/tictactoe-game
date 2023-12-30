e1bbf3a0a96c79efcaf41cf66a5f702312a3821b
nabinjana255 <nabinjana7@gmail.com>
2023-12-30 09:54:33 +0530

    moved NEW_GAME state outside of App Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { calculateWinner } from './winner';
 
 import './style.scss';
 
+const NEW_GAME = [{ squares: Array(9).fill(null), isXNext: false }]
+
 function App() {
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null), isXNext: false },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
 
   const gamingBoard = history[currentMove];
