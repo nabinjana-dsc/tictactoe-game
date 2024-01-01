be328feb3fd222c71dd52404b0254e56c8221cd6
nabinjana255 <nabinjana7@gmail.com>
2024-01-01 08:17:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ const App = () => {
       >
         Start New Game
       </button>
-      <h2 style={{ fontWeight: 'normal' }}>Current game history</h2>
+      <h2 style={{ fontWeight: 'normal' }}>Current gams history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
       <div className="bg-balls" />
     </div>
